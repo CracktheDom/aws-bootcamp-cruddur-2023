a102772aa1413acb9d658e7458e8206088fa83fb
CracktheDom <heymrdjd@gmail.com>
2023-03-13 07:19:58 +0000

    corrected syntax of Auth.currentAuthenticatUser() in HomeFeedPage.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -39,7 +39,7 @@ export default function HomeFeedPage() {
   const checkAuth = async () => {
     console.log('checkAuth')
     // [TODO] Authenication
-    Auth.currentAuthenicatedUser({
+    Auth.currentAuthenticatedUser({
       // Optional, By default is false.
       // If set to true, this call will send a 
       // request to Cognito to get the latest user data
