ede1ba1687199618f1b7cabf28706d8afd913da7
CracktheDom <85846263+CracktheDom@users.noreply.github.com>
2023-03-13 15:57:28 -0500

    Corrected syntax of onsubmit_confirm_code() in RecoverPage.js

--- frontend-react-js/src/pages/RecoverPage.js
+++ frontend-react-js/src/pages/RecoverPage.js
@@ -30,9 +30,9 @@ export default function RecoverPage() {
     if (password == passwordAgain){
       Auth.forgotPasswordSubmit(username, code, password)
       .then((data) => setFormState('success'))
-      .catch((err) => setCognitoErrors(err.message));
+      .catch((err) => setErrors(err.message));
     } else {
-      setCognitoErrors('Passwords do not match')
+      setErrors('Passwords do not match')
     }
     return false
   }
