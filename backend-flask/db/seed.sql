b6e4fa93f5753e243468d1ad792ce80c3398009f
CracktheDom <heymrdjd@gmail.com>
2023-03-24 07:12:16 +0000

    Create new mock data in seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,11 +1,11 @@
-insert into public.users (display_name, handle, cognito_user_id)
-values
-  ('Pop Goes the Weasel', 'hotwheels', 'MOCK'),
-  ('Fifty Pence', 'getFullOrDieTryin', 'MOCK');
+INSERT INTO public.users (display_name, handle, cognito_user_id, email)
+VALUES
+    ('Capt. Picard', 'the_captain', 'MOCK', 'Jean-luc@startrek.net'),
+    ('Geralt of Rivia', 'Blaviken_Butcher', 'MOCK', 'WhiteWolf@winterhome.com');
 
-insert into public.activities (user_uuid, message, expires_at)
-  values (
-    (select uuid from public.users where users.handle = 'hotwheels' limit 1),
-    'This was imported as seed data',
-    current_timestamp + interval '10 day'
-  );
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES (
+        (SELECT uuid FROM public.users WHERE users.handle = 'BlavikenButcher' LIMIT 1), 
+        'This was imported as seed data!!!', 
+        current_timestamp + interval '10 day'
+)
