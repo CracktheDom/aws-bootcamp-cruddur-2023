804dac77cd328f1b341b80138859bcbe208d4d59
CracktheDom <85846263+CracktheDom@users.noreply.github.com>
2023-05-16 15:57:37 -0500

    Update user email in seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id, email)
 VALUES
     ('Capt. Picard', 'the_captain', 'MOCK', 'Jean-luc@startrek.net'),
-    ('Geralt of Rivia', 'Blaviken_Butcher', 'MOCK', 'WhiteWolf@winterhome.com');
+    ('Geralt of Rivia', 'Blaviken_Butcher', 'MOCK', 'WhiteWolf@KaerMorhen.com');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES (
