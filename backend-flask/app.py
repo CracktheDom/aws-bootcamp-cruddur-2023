c77bef9b0d6f379ea0a6f533eb20dd9c07f9edb0
CracktheDom <heymrdjd@gmail.com>
2023-06-20 05:37:16 +0000

    Correct indentation in init_rollbar() in app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -112,14 +112,14 @@ def health_check():
 def init_rollbar():
   """init rollbar module"""
   rollbar.init(
-      # access token
-      rollbar_access_token,
-      # environment name
-      'development',
-      # server root directory, makes tracebacks prettier
-      root=os.path.dirname(os.path.realpath(__file__)),
-      # flask already sets up logging
-      allow_logging_basic_config=False)
+    # access token
+    rollbar_access_token,
+    # environment name
+    'development',
+    # server root directory, makes tracebacks prettier
+    root=os.path.dirname(os.path.realpath(__file__)),
+    # flask already sets up logging
+    allow_logging_basic_config=False)
 
   # send exceptions from `app` to rollbar, using flask's signal system.
   got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
