5c3309ce350e379d35912c3108362b853c74009c
CracktheDom <heymrdjd@gmail.com>
2023-07-06 08:04:02 +0000

    Correct SQL syntax in cruddur_post_confirmation.py

--- backend-flask/lambdas/cruddur_post_confirmation.py
+++ backend-flask/lambdas/cruddur_post_confirmation.py
@@ -8,25 +8,21 @@ def lambda_handler(event, context):
     print(f"Attributes: {user['name']}, {user['email']}, {user['preferred_username']}, {user['sub']}")
     try:
         print('entered try stmt')
-        sql = f"""INSERT INTO users (display_name, handle, email, cognito_user_id)
-                    VALUES (
-                    '{user['name']}',
-                    '{user['preferred_username']}',
-                    '{user['email']}',
-                    '{user['sub']}');
-                    )"""
-        print("Attempting psycopg connection...")
-        conn = psycopg2.connect(os.getenv("PROD_CONNECTION_URL"))
-        print("psycopg connection established")
+        sql = f"""INSERT INTO public.users (display_name, handle, email, cognito_user_id)
+                    VALUES (%s, %s, %s, %s)"""
+        conn_url = os.getenv("PROD_CONNECTION_URL")
+        print("Attempting psycopg2 connection to ...")
+        conn = psycopg2.connect(conn_url)
+        print("psycopg2 connection established")
         cur = conn.cursor()
-        print(f"cursor object: {cur} \n")
-        cur.execute(sql)
+        cur.execute(sql, (user['name'], user['preferred_username'], user['email'], user['sub']))
         conn.commit()
     except psycopg2.OperationalError as error:
         print(f"Unable to establish connection:\n {print_psycopg2_exception(error)}")
         conn = None
     except (Exception, psycopg2.DatabaseError) as error:
         print_psycopg2_exception(error)
+        conn = None
     finally:
         # if connection was established, then it will be closed
         if conn:
@@ -44,7 +40,7 @@ def print_psycopg2_exception(err):
     line_num = traceback.tb_lineno
 
     # print the connect() error
-    print(f"\npsycopg2 ERROR: {err} on lime number: {line_num}")
+    print(f"\npsycopg2 ERROR: {err} on line number: {line_num}")
     print(f"psycopg2 traceback: {traceback} -- type {err_type}")
 
     # psycopg2 extensions.Diagnostics object attribute
